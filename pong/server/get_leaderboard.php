5837f3d57aa05064114d4ba0cff2d496576ef60c
Dzsanga <119756283+DzsangaGIT@users.noreply.github.com>
2025-02-26 09:38:16 +0100

    Delete pong directory

--- pong/server/get_leaderboard.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$conn = new mysqli("localhost", "root", "", "pong_game"); 
-$result = $conn->query("SELECT * FROM leaderboard ORDER BY total_wins DESC, total_score DESC LIMIT 100"); 
-$leaderboard = []; 
-
-while ($row = $result->fetch_assoc()) { $leaderboard[] = $row; 
-} 
-echo json_encode($leaderboard); 
-?>
