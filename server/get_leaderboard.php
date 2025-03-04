a9dfb95c95757646e0829397d3c0071c9d638026
Dzsanga <119756283+DzsangaGIT@users.noreply.github.com>
2025-03-04 19:19:41 +0100

    Update get_leaderboard.php

--- server/get_leaderboard.php
+++ server/get_leaderboard.php
@@ -1,4 +1,15 @@
 <?php 
+
+// CREATE DATABASE pong_game; 
+
+// USE pong_game; 
+
+// CREATE TABLE leaderboard (
+//     id INT AUTO_INCREMENT PRIMARY KEY, 
+//     player_name VARCHAR(255) NOT NULL, 
+//     score INT NOT NULL 
+// );
+
 $conn = new mysqli("localhost", "root", "", "pong_game"); 
 $result = $conn->query("SELECT * FROM leaderboard ORDER BY total_wins DESC, total_score DESC LIMIT 100"); 
 $leaderboard = []; 
